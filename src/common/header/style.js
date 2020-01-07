38c7bbce7d55d90199e9fc8a3f194f1be4f810ca
wb-lm579891 <wb-lm579891@autonavi.com>
2020-01-07 10:16:52 +0800

    first commit

---
+++ src/common/header/style.js
@@ -0,0 +1,83 @@
+import styled from 'styled-components';
+import logoPic from '../../static/logo.png'
+export const HeaderWrapper=styled.div`
+  height:58px;
+  border-bottom:1px solid #F0F0F0;
+  `
+export const Logo=styled.a.attrs({
+  href:"/"   // 点击简书logo调到主页  
+})`
+  position:absolute;
+  top:0;
+  left:0;
+  display:block; // a标签需要设置宽高 设置为block
+  height:56px;
+  width:100px;
+  background:url(${logoPic}); // 引入图片
+  background-size:contain;
+`
+export const Nav=styled.div`
+  width:960px;
+  height:100%;
+  margin:0 auto;
+  padding-right:70px;
+  box-sizing:border-box;
+`
+export const NavItem=styled.div`
+line-height:56px;
+padding:0 15px;
+font-size:17px;
+color:#333;
+  &.left{ // 如果当前是NavItem组件 同时NavItem组件有left，那么向左浮动
+    float:left;
+  }
+  &.right{
+    float:right;
+    color:#969696;
+  }
+  &.active{
+    color:#ea6f5a;
+  }
+`
+export const NavSearch=styled.input.attrs({
+  placeholder:'搜索'
+})`
+  width:240px;
+  height:38px;
+  margin-top:9px;// 56-38=18/2=9
+  margin-left:20px;
+  padding:0 20px;
+  box-sizing:border-box;// box-sizing https://developer.mozilla.org/zh-CN/docs/Web/CSS/box-sizing
+  border:none;
+  outline:none;
+  border-radius:19px;// https://developer.mozilla.org/zh-CN/docs/Web/CSS/border-radius
+  background:#eee;
+  font-size:14px;
+  &:placeholder{
+    color:#999;
+  }
+`
+
+export const Addition=styled.div`
+  position:absolute;
+  right:0;
+  top:0;
+`
+
+export const Button=styled.div`
+  float:right;
+  margin-top:9px;
+  margin-right:20px;
+  padding:0 20px;
+  line-height:38px;
+  border-radius:19px;
+  border:1px solid #ec6149;
+  fonnt-size:14px;
+  &.reg{
+    color:#ec6149
+  }
+  &.writting{
+    color:#fff;
+    background:#ec6149;
+  }
+`
---
+++ src/style.js
@@ -0,0 +1,50 @@
+// 错误是因为styled-components废弃injectGlobal这个API，更改为createGlobalStyle。用法如下：
+// https://blog.csdn.net/qq_36242361/article/details/89308068
+
+// https://meyerweb.com/eric/tools/css/reset/ 引用reset.css
+import { createGlobalStyle } from 'styled-components';
+export const GlobalStyle = createGlobalStyle`
+html, body, div, span, applet, object, iframe,
+h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+a, abbr, acronym, address, big, cite, code,
+del, dfn, em, img, ins, kbd, q, s, samp,
+small, strike, strong, sub, sup, tt, var,
+b, u, i, center,
+dl, dt, dd, ol, ul, li,
+fieldset, form, label, legend,
+table, caption, tbody, tfoot, thead, tr, th, td,
+article, aside, canvas, details, embed, 
+figure, figcaption, footer, header, hgroup, 
+menu, nav, output, ruby, section, summary,
+time, mark, audio, video {
+	margin: 0;
+	padding: 0;
+	border: 0;
+	font-size: 100%;
+	font: inherit;
+	vertical-align: baseline;
+}
+/* HTML5 display-role reset for older browsers */
+article, aside, details, figcaption, figure, 
+footer, header, hgroup, menu, nav, section {
+	display: block;
+}
+body {
+	line-height: 1;
+}
+ol, ul {
+	list-style: none;
+}
+blockquote, q {
+	quotes: none;
+}
+blockquote:before, blockquote:after,
+q:before, q:after {
+	content: '';
+	content: none;
+}
+table {
+	border-collapse: collapse;
+	border-spacing: 0;
+}
+`
